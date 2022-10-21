2f8d16fc78a9d1433bf39e2a773927b52928becb
Vikram <vikstorm345@gmail.com>
2022-10-21 12:21:05 -0400

    Multithread

--- HTTP_Server.py
+++ HTTP_Server.py
@@ -1,8 +1,28 @@
-# import socket module
 from socket import *
-
-
+from threading import Thread
 import sys  # In order to terminate the program
+
+
+class ConnectionThread(Thread):
+
+    def __init__(self, IP, PORT):
+        Thread.__init__(self)
+        self.IP = IP
+        self.PORT = PORT
+
+    def run(self):
+        while True:
+            message = connectionSocket.recv(1024)
+            filename = message.split()[1]
+            print(filename)
+            f = open(filename[1:])
+            outputdata = f.read()
+
+            connectionSocket.send(('HTTP/1.1 200 OK\r\n\r\n').encode())
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -12,43 +32,26 @@ HOST = '10.10.63.19'
 PORT = 8765
 serverSocket.bind((HOST, PORT))
 serverSocket.listen(1)
+
+threads = []
 # Fill in end
 print('Ready to serve...')
 while True:
-    # Establish the connection
-
 
     connectionSocket, addr = serverSocket.accept()
     print('  Local Server:  Accepting connection to ' + str(addr))
-    size = 1024
+
     try:
-        message = connectionSocket.recv(size)
-
-        print(message)
-
-        filename = message.split()[1]
-        print("file " + str(filename[1:]))
-        f = open(filename[1:])
-        outputdata = f.read()  # Fill in start #Fill in end
-        print("here")
-        # Send one HTTP header line into socket
-        # Fill in start
-        connectionSocket.send(('HTTP/1.1 200 OK\r\n\r\n').encode())
-        # Fill in end
-        # Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
+        nextThread = ConnectionThread(addr[0], addr[1])
+        nextThread.start()
+        threads.append(nextThread)
 
         connectionSocket.close()
     except IOError:
-        # Send response message for file not found
-        # Fill in start
-        connectionSocket.send(("404 NOT FOUND").encode())
-        # Fill in end
-        # Close client socket
-        # Fill in start
+        connectionSocket.send(("HTTP/1.1 404 Not Found\r\n\r\n").encode())
         connectionSocket.close()
-        # Fill in end
+for thread in threads:
+    thread.join()
+
 serverSocket.close()
 sys.exit()  # Terminate the program after sending the corresponding data
